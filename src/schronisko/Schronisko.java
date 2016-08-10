c1854d395c2de71df18cdbc5fe12272632d76205
ASUS <ASUS@ASUS-PC>
2016-08-10 22:13:14 +0200

    new commit

--- src/schronisko/Schronisko.java
+++ src/schronisko/Schronisko.java
@@ -6,33 +6,31 @@
 package schronisko;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 import java.util.Scanner;
-
 
 /**
  *
  * @author ASUS
  */ 
-public class Schronisko {
+public class Schronisko {           //everything's pubic for now
 
-    public String type;
+    public String type;             // the type of animal to fill the list
+    public String removeAnimal;     //the String used when removing elements from the list
     public ArrayList<String> numberOfAnimals = new ArrayList<>();
     
     public void addAnimal(String type){
         this.type=type;
         Scanner in = new Scanner(System.in);
-        System.out.println("\n     Please give the animal type: ");
+        System.out.print("     Please give the animal type: ");
         type = in.nextLine();
-        System.out.println("\n     You have entered: " + type);
+        System.out.println("     You have entered: " + type);
         numberOfAnimals.add(type);
     }
    
-    
-    public void Status(){
-        System.out.println("\n     Total number of animals: " + numberOfAnimals.size());
-//        numberOfAnimals.forEach(System.out::println);
-//        System.out.println(numberOfAnimals);
-        
+    public void Status(){       //Status method to see hpw many elements there're on the list
+        System.out.println("     Total number of animals: " + numberOfAnimals.size());
         for (int i=0;i<numberOfAnimals.size();i++) {
             System.out.println(numberOfAnimals.get(i));
         }
@@ -59,7 +57,7 @@ public class Schronisko {
         Scanner in = new Scanner(System.in);
         
         int choice = menu();
-        Schronisko shelter1 = new Schronisko();
+        Schronisko shelter1 = new Schronisko();                  //create a new object from my shelter class
         
         while(choice!=0) {
             switch(choice){
@@ -67,21 +65,40 @@ public class Schronisko {
                     shelter1.addAnimal("");
                     break;
                 case 2:
-                    int i;
-                    System.out.println("\n     How many Animals would you like to add?");
-                    i = in.nextInt();
+                    int i = 0;
+                    System.out.print("     Enter how many Animals you would like to add: ");
+                    try {
+                        i = in.nextInt();
+                    } catch (Exception e) {
+                        System.out.println("    Please enter only numeric values.");
+                    }
                         
                     while (i>0){
-                    shelter1.addAnimal("");
-                    i=i-1;
+                        shelter1.addAnimal("");
+                        i=i-1;
                     }
-                    break;
+                break;
+                
                 case 3:
-                    System.out.println("\n     Which animal would you like to remove from your list?");
-                    System.out.println(shelter1.numberOfAnimals);
-//                    shelter1.numberOfAnimals.forEach(System.out::println);
-                    String removeAnimal = in.nextLine();
-                    shelter1.numberOfAnimals.remove(removeAnimal);
+                    System.out.println("     Which animal would you like to remove from your list?");
+                    
+                    Iterator iterator = shelter1.numberOfAnimals.iterator();        //used an Iterator to go through the elements
+                    while(iterator.hasNext()){                                      //could've used a for loop,
+                        Object element = iterator.next();
+                        System.out.println(element);
+                    }
+                    Scanner in2 = new Scanner (System.in);
+                    shelter1.removeAnimal = in2.nextLine();
+                    ListIterator listIter = shelter1.numberOfAnimals.listIterator();  //used a listIterator to modify the list
+                        while(listIter.hasNext()){
+                            if(listIter.next().equals(shelter1.removeAnimal)){
+                                listIter.remove();
+                            System.out.println("    You have removed: " +shelter1.removeAnimal ); 
+                            }
+                            else if(!(shelter1.numberOfAnimals.contains(shelter1.removeAnimal)));
+                                System.out.println("    There is no " + "\"" + shelter1.removeAnimal+"\"" + " on the list");
+                                }
+                                                  
                     
                     break;
                 case 4:
